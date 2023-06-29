117a9789b9b54621789b191ec64e966e86ac15d8
s20343 <s20343@pjwstk.edu.pl>
2023-06-29 21:57:05 +0200

    First Commit

--- src/Task1/task/Main.java
+++ src/Task1/task/Main.java
@@ -10,7 +10,7 @@ public class Main {
         Producer ford=new Producer("Ford");
         Producer jdcecaux=new Producer("JCDecaux");
 
-       Bus bus1=new Bus(mercedes,LocalDate.of(2016,Month.SEPTEMBER,6),true,false,Fuel.OIL,new BigDecimal("6.0"));
+       Bus bus1=new Bus(mercedes,LocalDate.of(2015,Month.SEPTEMBER,6),true,false,Fuel.OIL,new BigDecimal("6.0"));
        Bus bus2=new Bus(ford,LocalDate.of(1985,Month.FEBRUARY,25),false,true,Fuel.GAS,new BigDecimal("4.0"));
        Tram tram1=new Tram(jdcecaux,LocalDate.of(2018,Month.MAY,10),true,true,Fuel.ELECTRICITY,new BigDecimal("0"));
        Tram tram2=new Tram(jdcecaux,LocalDate.of(2008,Month.AUGUST,20),false,false,Fuel.ELECTRICITY,new BigDecimal("0"));
